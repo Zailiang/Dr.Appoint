d785e008cf7313df47d21f244c748e0e4bf6330f
Zick Chao <Zick.Chaos@hotmail.com>
2016-02-16 15:26:55 -0600

    collection view and table view prototype

---
+++ Dr. Appoint/customCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  customCollectionViewCell.h
+//  Dr. Appoint
+//
+//  Created by Zick Chao on 2/16/16.
+//  Copyright © 2016 Zailiang Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface customCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Dr. Appoint/customCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  customCollectionViewCell.m
+//  Dr. Appoint
+//
+//  Created by Zick Chao on 2/16/16.
+//  Copyright © 2016 Zailiang Yu. All rights reserved.
+//
+
+#import "customCollectionViewCell.h"
+
+@implementation customCollectionViewCell
+
+@end
